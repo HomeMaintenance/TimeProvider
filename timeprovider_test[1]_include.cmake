e7ca3ec78e57283e67cf157770d3d06c6106c701
Planet-Moon <topwastaken@abwesend.de>
2022-07-27 23:33:39 +0200

    first commit

---
+++ timeprovider_test[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "E:/Users/Andreas/Andreas/PrivateElektronik/Cpp/HomeMaintenance/TimeProvider/build/test/timeprovider_test[1]_tests.cmake")
+  include("E:/Users/Andreas/Andreas/PrivateElektronik/Cpp/HomeMaintenance/TimeProvider/build/test/timeprovider_test[1]_tests.cmake")
+else()
+  add_test(timeprovider_test_NOT_BUILT timeprovider_test_NOT_BUILT)
+endif()
